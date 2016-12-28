84cb6adada9dba3ecfb2b56f7224e5e361a5987f
Vasileios Vrouliotis <thelocodmw2@gmail.com>
2016-12-28 23:02:00 +0200

    Basic organizement.

---
+++ Molecules/mainSample.hpp
@@ -0,0 +1,14 @@
+#include <bitset>
+// #include "NodeFunctions.cpp"
+// #include "ListsFunctions.cpp"
+// #include "VectorHammingListManagement.cpp"
+// #include "HammingListManagement.cpp"
+// #include "HashFunctions.cpp"
+// #include "DBListManagement.cpp"
+// #include "DBHManagement.cpp"
+#include "Headers.hpp"
+// #include "UtilsH.h"
+// #include "Algorithms.h"
+
+// void CLI(ifstream& inputFile, ofstream& outputFile, Conf* myConf, Metrics* myMetric);
+// void ReadFiles(ifstream& inputFile, Metrics* myMetric, int* N, double** distanceMatrix);
